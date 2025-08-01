3bdef964d4ef8684d8350bbc3243a891c6d5835b
Varshita Nukala <98635420+nukalavarshita@users.noreply.github.com>
2025-08-01 12:44:05 +0530

    Update build_and_push.sh

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -1,22 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-# Exit immediately if a command fails
-set -e
-
-# Variables
 IMAGE_NAME="varshitanukala/nodeapp"
 TAG="latest"
 
-# Login to DockerHub
 echo "Logging in to DockerHub..."
-echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# Build Docker image
 echo "Building Docker image..."
-docker build -t $IMAGE_NAME:$TAG myapp/
+docker build -t "$IMAGE_NAME:$TAG" myapp/
 
-# Push Docker image
 echo "Pushing Docker image to DockerHub..."
-docker push $IMAGE_NAME:$TAG
+docker push "$IMAGE_NAME:$TAG"
 
 echo "Docker image pushed successfully!"
+
+
+
+# #!/bin/bash
+
+# # Exit immediately if a command fails
+# set -e
+
+# # Variables
+# IMAGE_NAME="varshitanukala/nodeapp"
+# TAG="latest"
+
+# # Login to DockerHub
+# echo "Logging in to DockerHub..."
+# echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+
+# # Build Docker image
+# echo "Building Docker image..."
+# docker build -t $IMAGE_NAME:$TAG myapp/
+
+# # Push Docker image
+# echo "Pushing Docker image to DockerHub..."
+# docker push $IMAGE_NAME:$TAG
+
+# echo "Docker image pushed successfully!"
